a5bebb7579fe5359e46ee045dc7fe09d36e08fbb
Ather Hussen Murad <muradnowshad20@gmail.com>
2026-05-02 22:08:40 +0600

    type interface

---
+++ Module 2 - Explore Advance Types of TypeScript/My self/type-interface.ts
@@ -0,0 +1,52 @@
+type User = {
+  name: string;
+  age: number;
+};
+type Role = {
+  role: "admin" | "user";
+};
+
+type UserWithRole = User & Role;
+
+interface IuserWithRole extends IUser {
+  role: "admin" | "user";
+}
+const user1: User = {
+  name: "Murad",
+  age: 100,
+};
+const user2: UserWithRole = {
+  name: "Momshad",
+  age: 102,
+  role: "user",
+};
+
+//  type Interface below
+// interface only work in object type dataType like:- array, object and function
+interface IUser {
+  name: string;
+  age: number;
+}
+
+type Add = (num1: number, num2: number) => number;
+// const add: Add = (num1, num2) => num1 + num2;
+
+// use interface for function
+
+interface IAdd {
+  (num1: number, num2: number): number;
+}
+const add: Add = (num1, num2) => num1 + num2;
+
+// .............
+
+type Friends = string[];
+// const friedns: string[] = ["a", "b", "c"];
+// const friends: Friends = ["a", "b", "c"];
+
+interface IFriends {
+  [index: number]: string; //indexing.
+}
+const friends: IFriends = ["a", "b", "c"];
+
+// kokon kunta use korbo

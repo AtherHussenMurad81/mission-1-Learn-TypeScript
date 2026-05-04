8aede8baf877339b8d63071068119c59bd125445
Ather Hussen Murad <muradnowshad20@gmail.com>
2026-05-04 19:26:06 +0600

    key of constraint

--- Module 2 - Explore Advance Types of TypeScript/My self/src/key_of_constraint.ts
+++ Module 2 - Explore Advance Types of TypeScript/My self/src/key_of_constraint.ts
@@ -9,7 +9,56 @@ type RichPeopleVehicle = {
   cycle: string;
 };
 
-type vehicle1 = "car" | "bike" | "cycle";
+type Vehicle1 = "car" | "bike" | "cycle";
 // ? keyof operator er maddome
 
-type vehicle2 = keyof RichPeopleVehicle;
+type Vehicle2 = keyof RichPeopleVehicle;
+// also
+
+// const myVehicle: Vehicle1 = "motor"; //also not accepted
+// const myVehicle2: Vehicle2 = "motor"; // not accepted
+
+// ? key of constraint
+
+type User = {
+  id: number;
+  name: string;
+  address: {
+    city: string;
+  };
+};
+const user: User = {
+  id: 123,
+  name: "murad",
+  address: {
+    city: "Sylhet",
+  },
+};
+
+// const myId = user.id;
+// const myId2 = user["id"];
+// console.log({ myId, myId2 });
+
+// const getPropertyFromObj = (
+//   obj: User,
+//   key: keyof User | "name" | "address",
+// ) => {
+//   return obj[key];
+
+//? error => cannot find key. I means ID... arrow put dowm for User type used || Keyof User
+// };
+const getPropertyFromObj = <X>(obj: X, key: keyof X) =>
+  //?   | "name" | "address")
+  {
+    return obj[key];
+
+    //? error => cannot find key. I means ID... arrow put dowm for User type used || Keyof User
+  };
+const result3 = getPropertyFromObj(user, "name");
+console.log(result);
+
+const product = {
+  brand: "HP",
+};
+
+const result5 = getPropertyFromObj(product, "brand"); //its cannot work for defind User type

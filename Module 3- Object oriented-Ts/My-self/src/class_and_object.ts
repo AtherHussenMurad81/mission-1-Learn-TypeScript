ee677e8de4995a6690fe921600e1e1bd03e9fb3f
Ather Hussen Murad <muradnowshad20@gmail.com>
2026-05-05 20:29:54 +0600

    object parameter properties

--- Module 3- Object oriented-Ts/My-self/src/class_and_object.ts
+++ Module 3- Object oriented-Ts/My-self/src/class_and_object.ts
@@ -1,1 +1,60 @@
-console.log("ikujhgakjlhagjkh");
+// ? opp =- class - object
+
+// * property define kora hoise
+// class Animal {
+//   name: string;
+//   species: string;
+//   sound: string;
+//   constructor(name: string, species: string, sound: string) {
+//     this.name = name;
+//     this.species = species;
+//     this.sound = sound;
+//   }
+// }
+// const dog = new Animal("doges", "dogsp", "ghew ghew");
+// dog.name;
+// // console.log(dog); //? Output: Animal { name: 'doges', species: 'dogsp', sound: 'ghew ghew' }
+
+// const cat = new Animal("cat bhai", "cet", "mew mew");
+// console.log(cat);
+
+//* ....................
+
+// class Animal {
+//   name: string;
+//   species: string;
+//   sound: string;
+//   constructor(name: string, species: string, sound: string) {
+//     this.name = name;
+//     this.species = species;
+//     this.sound = sound;
+//   }
+//   //* its called class method
+//   makeSound() {
+//     console.log(`the ${this.name} is making sound ${this.sound} `);
+//   }
+// }
+// const cat = new Animal("cat bhai", "cet", "mew mew");
+// const dog = new Animal("doges", "dogsp", "ghew ghew");
+// console.log(cat.makeSound()); //undefined
+
+// dog.makeSound();
+
+// ? Parameter properties
+
+class Animal {
+  constructor(
+    public name: string,
+    public species: string,
+    public sound: string,
+  ) {}
+
+  makeSound() {
+    console.log(`${this.name} is making sound: ${this.sound}`);
+  }
+}
+const cat = new Animal("mruad", "mom", "now");
+// console.log(object);
+// cat.makeSound();
+console.log(cat);
+cat.name;
